eabf2d8c55be7887900d70d3f519e7df18851511
Yantik <Foxmen12345678910@mail.ru>
2022-12-25 21:27:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "two varrible" << endl;
+	cin >> a >> b;
+	while (true)
+	{
+		if (a % b == 0)
+		{
+			cout << "NOD = " << b<< endl;
+			break;
+		}
+		else
+		{
+			b = a % b;
+		}
+		
+	}
+}
